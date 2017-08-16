0e4cef7503eeb97b078287b4a90c5962e2cde5e7
David García Alonso <g.a.david@gmail.com>
2017-08-17 00:44:09 +0200

    Antes de añadir el form en login.html - p90

--- blog.py
+++ blog.py
@@ -7,6 +7,9 @@ import sqlite3
 
 # Configuación
 DATABASE = 'blog.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+SECRET_KEY = b'L\xe8Q\x93Y\xb4\x86rn(\xad\xeex7\x8ck\x03umD\x9e"r#'
 
 
 app = Flask(__name__)
@@ -20,9 +23,20 @@ def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    error = None
+    status_code = 200
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME'] or request.form['passowrd'] != app.config['PASSWORD']:
+            error = 'Credenciales no válidas. Pruebe de nuevo.'
+            status_code = 401
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('main'))
+
+    return render_template('login.html', error=error), status_code
+
 
 @app.route('/main')
 def main():
