0512e1e7d9539d0d56493d52ff9509375b9a6957
David García Alonso <g.a.david@gmail.com>
2017-08-17 14:36:48 +0200

    Trabajando en login.html

--- blog.py
+++ blog.py
@@ -40,7 +40,7 @@ def login():
 
 @app.route('/main')
 def main():
-    return  render_template('main.html')
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
