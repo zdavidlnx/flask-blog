d8aab1e9e7e517a57868c373bb581ba0472e68bf
David García Alonso <g.a.david@gmail.com>
2017-08-23 01:06:26 +0200

    Show postd - p98

--- blog.py
+++ blog.py
@@ -70,7 +70,19 @@ def main():
 @app.route('/add', methods=['POST'])
 @login_required
 def add():
-    pass
+    title = request.form['title']
+    post = request.form['post']
+    if not title or not post:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('main'))
+    else:
+        g.db = connect_db()
+        g.db.execute('insert into posts (title, post) values (?, ?)',
+                     [request.form['title'], request.form['post']])
+        g.db.commit()
+        g.db.close()
+        flash('New entry was successfully posted!')
+        return redirect(url_for('main'))
 
 
 if __name__ == '__main__':
